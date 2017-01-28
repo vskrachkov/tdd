aaab58e854f1ea6f78277c15795fceb78ec28591
Vyacheslav Krachkov <vskrachkov@gmail.com>
2017-01-28 23:14:39 +0200

    home_page with minimal html added

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+# from django.contrib import admin
+
+import lists.views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', lists.views.home_page, name='home_page'),
 ]
